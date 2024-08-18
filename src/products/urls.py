a803c9db429f7fac1f68ea753f5b6074a1958f40
Aaron Cymor Nemeno <aaroncymor@gmail.com>
2024-08-19 06:21:32 +0800

    Removed redirected

--- src/products/urls.py
+++ src/products/urls.py
@@ -5,7 +5,5 @@ app_name = "products"
 
 urlpatterns = [
     path('', views.product_list, name='list'),
-    path('verify', views.verify, name='verify'),
-    path('redirected', views.redirected, name='redirected'),
     path('create_or_edit', views.product_create_or_edit, name='create_or_edit'),
 ]
